e9e9300f9c53db75e0307ea301a771ebba74d95c
ASRAI <ASRAI@ASRAI-HP>
2017-09-12 10:15:38 +0530

    This is the first copy

---
+++ validation.testcases.web/src/test/java/web/validation/testcases/PrintChar.java
@@ -0,0 +1,16 @@
+package web.validation.testcases;
+
+public class PrintChar {
+public void starStar()
+{int i=0;
+	while(i<20){
+		System.out.println(i);
+		i++;
+	}
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+new PrintChar().starStar();
+	}
+
+}
